0b0476f057161e35801efde8491dafff69081a83
6henrykim <22160472+6henrykim@users.noreply.github.com>
2020-03-25 00:27:19 -0500

    Added script for saving the screenshot
    The script is a component of the ScriptHolder object which is accessed by the camera button. I also had to change the player settings for write from internal to external

---
+++ BillboARd/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Screenshot : MonoBehaviour
+{
+    string galleryName = "BillboARd";
+    int screenHeightCropped;
+    string imageName;
+
+    public void TakeScreenshot()
+    {
+
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            TakeScreenshotAndSaveWindowsTest();
+        }
+
+        StartCoroutine(TakeScreenshotAndSave());
+
+    }
+
+
+    private IEnumerator TakeScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+
+        screenHeightCropped = (int)(Screen.height * 0.9f);
+        Texture2D ss = new Texture2D(Screen.width, screenHeightCropped, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, (Screen.height - screenHeightCropped), Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        // Save the screenshot to Gallery/Photos
+        imageName = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        NativeGallery.SaveImageToGallery(ss, galleryName, imageName);
+
+        // To avoid memory leaks
+        Destroy(ss);
+    }
+
+    private void TakeScreenshotAndSaveWindowsTest()
+    {
+        ScreenCapture.CaptureScreenshot("C:\\BillboARdScreenshots\\screenshot.png");
+    }
+
+}
