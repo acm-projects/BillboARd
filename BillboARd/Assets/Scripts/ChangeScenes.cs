5927299f169e09e267d18cdf7bf9d6f7a49ea467
6henrykim <22160472+6henrykim@users.noreply.github.com>
2020-04-05 19:26:10 -0500

    Added back button to Gallery scene

---
+++ BillboARd/Assets/Scripts/ChangeScenes.cs
@@ -0,0 +1,48 @@
+﻿//This script lets you load a Scene asynchronously. It uses an asyncOperation to calculate the progress and outputs the current progress to Text (could also be used to make progress bars).
+
+//Attach this script to a GameObject
+//Create a Button (Create>UI>Button) and a Text GameObject (Create>UI>Text) and attach them both to the Inspector of your GameObject
+//In Play Mode, press your Button to load the Scene, and the Text changes depending on progress. Press the space key to activate the Scene.
+//Note: The progress may look like it goes straight to 100% if your Scene doesn’t have a lot to load.
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScenes : MonoBehaviour
+{
+    // These will be set up in the inspector
+    public string m_NextSceneName;
+
+
+    public void LoadNextScene()
+    {
+        //Start loading the Scene asynchronously and output the progress bar
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(m_NextSceneName);
+
+        //Don't let the Scene activate until you allow it to
+        //Gives foundation for delaying scene from loading such as for an animation to play
+        asyncOperation.allowSceneActivation = false;
+
+        while (!asyncOperation.isDone)
+        {
+           
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
