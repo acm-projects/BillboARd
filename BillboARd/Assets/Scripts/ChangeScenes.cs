37cb12d577a336060092c779630669740ed04eac
6henrykim <22160472+6henrykim@users.noreply.github.com>
2020-04-05 20:41:44 -0500

    Added script for changing scenes
    The user can now switch between the Gallery scene and ARTargetScene

--- BillboARd/Assets/Scripts/ChangeScenes.cs
+++ BillboARd/Assets/Scripts/ChangeScenes.cs
@@ -1,44 +1,44 @@
-﻿//This script lets you load a Scene asynchronously. It uses an asyncOperation to calculate the progress and outputs the current progress to Text (could also be used to make progress bars).
-
-//Attach this script to a GameObject
-//Create a Button (Create>UI>Button) and a Text GameObject (Create>UI>Text) and attach them both to the Inspector of your GameObject
-//In Play Mode, press your Button to load the Scene, and the Text changes depending on progress. Press the space key to activate the Scene.
-//Note: The progress may look like it goes straight to 100% if your Scene doesn’t have a lot to load.
-
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class ChangeScenes : MonoBehaviour
 {
-    // These will be set up in the inspector
-    public string m_NextSceneName;
+    public Button m_SceneButton; //Array of buttons to be linked to in inspector
+    public string m_SceneName;  //Array of names of scenes to change to specified in inspector
 
 
-    public void LoadNextScene()
+    void Start()
     {
-        //Start loading the Scene asynchronously and output the progress bar
-        StartCoroutine(LoadSceneAsync());
+        //Call the LoadButton() function when the user clicks this Button
+        m_SceneButton.onClick.AddListener(() => LoadButton(m_SceneName));
     }
 
-    IEnumerator LoadSceneAsync()
+    void LoadButton(string nextSceneName)
+    {
+        //Start loading the Scene asynchronously and output the progress bar
+        StartCoroutine(LoadScene(nextSceneName));
+    }
+
+    IEnumerator LoadScene(string nextSceneName)
     {
         yield return null;
 
         //Begin to load the Scene you specify
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(m_NextSceneName);
-
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(nextSceneName);
         //Don't let the Scene activate until you allow it to
-        //Gives foundation for delaying scene from loading such as for an animation to play
         asyncOperation.allowSceneActivation = false;
 
+        //When the load is still in progress, output the Text and progress bar
         while (!asyncOperation.isDone)
         {
-           
+
             // Check if the load has finished
             if (asyncOperation.progress >= 0.9f)
             {
+                //Activate the Scene
                 asyncOperation.allowSceneActivation = true;
             }
 
