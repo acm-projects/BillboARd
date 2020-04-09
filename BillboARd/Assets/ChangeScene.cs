f148638975b3d25901bc5d8389ccde0cd5860da6
6henrykim <22160472+6henrykim@users.noreply.github.com>
2020-04-08 23:12:28 -0500

    Initial import of package
    I did not import invisible.cs since that gave Stephen problems with building the app. I also deleted empty scenes and renamed the imported scenes to fit what the script in the Gallery scene accesses.

---
+++ BillboARd/Assets/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void changeMenuScene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
---
+++ BillboARd/Assets/GoToURL.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToURL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void btnTrailer()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=hAUTdjf9rko");
+    }
+
+    public void btnReview()
+    {
+        Application.OpenURL("https://www.rottentomatoes.com/m/it_2017");
+    }
+
+    public void btnShowtimes()
+    {
+        Application.OpenURL("https://www.fandango.com/it-2017-199151/movie-overview");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
