29c01d14abe84354a61e27a394eb780c12cb820d
6henrykim <22160472+6henrykim@users.noreply.github.com>
2020-04-20 00:26:26 -0500

    Add script that activates plane detection
    Plane detection and object placement should only be active once the Retail Poster has been scanned

---
+++ BillboARd/Assets/Scripts/ActivateObjectPlacement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjectPlacement : MonoBehaviour
+{
+    public GameObject m_PlaneParent;
+
+    public void ActivatePlaneDetection()
+    {
+        m_PlaneParent.SetActive(true);
+    }
+
+    public void DeactivatePlaneDetection()
+    {
+        m_PlaneParent.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlaneParent.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
